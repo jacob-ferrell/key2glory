296d6ff6efec847640349e2b169657046779aef5
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-26 13:22:16 -0400

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //content: ["./src/**/*.{html,ts,tsx}"],
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
