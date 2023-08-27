089fe5c8d6585b6913bf223cce4467f42ff15b26
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-27 15:44:17 -0400

    added copy/paste detection and added ref for focusing on input

--- src/components/GameInput.tsx
+++ src/components/GameInput.tsx
@@ -1,17 +1,26 @@
-import { ChangeEvent, SetStateAction } from "react";
+import { ChangeEvent, ClipboardEvent } from "react";
 
 type GameInputProps = {
-    userInput: string;
-    setUserInput: React.Dispatch<SetStateAction<string>>;
-    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-}
+  userInput: string;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleCopyOrPaste: (e: ClipboardEvent<HTMLInputElement>) => void;
+  inputRef: React.RefObject<HTMLInputElement>;
+};
 
-export default function GameInput({userInput, setUserInput, handleChange}: GameInputProps) {
-
-
-
-
-    return (
-        <input onChange={handleChange} defaultValue={userInput} className="w-full h-12 px-3 rounded"></input>
-    );
+export default function GameInput({
+  userInput,
+  handleChange,
+  handleCopyOrPaste,
+  inputRef,
+}: GameInputProps) {
+  return (
+    <input
+      onChange={handleChange}
+      onCopy={handleCopyOrPaste}
+      onPaste={handleCopyOrPaste}
+      ref={inputRef}
+      value={userInput}
+      className="w-full h-12 px-3 rounded"
+    ></input>
+  );
 }
