1adb188686f48986bef941b6e3fcd4b2d9f080b1
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-28 13:48:52 -0400

    refactored rending of text

--- src/pages/Game/GameTextDisplay.tsx
+++ src/pages/Game/GameTextDisplay.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import "../index.css";
 import ConfettiExplosion from "react-confetti-explosion";
 
 type GameTextDisplayProps = {
@@ -20,21 +19,16 @@ export default function GameTextDisplay({
   return (
     <div className="flex flex-col justify-center items-center">
         {isExploading ? <ConfettiExplosion force={0.6} duration={2500} particleCount={80} width={1000} /> : null}
-        <div className="text-2xl flex">
-          {matches.map((match, index) => {
-            return  (
-              <>
-                <div
-                  key={index}
-                  className={match ? "grow text-green-500" : "wiggle     text-red-500"}
-                >
-                  {text[index]}
-                </div>
-                {text[index + 1] === " " ? <span>&nbsp;</span> : null}
-              </>
-            );
+        <div className="text-2xl flex flex-wrap">
+          {text.split("").map((char, i) => {
+            char = char == " " ? "\u00A0" : char;
+            if (i > matches.length - 1) {
+              return <span className="text-grey-800">{char}</span>
+            }
+            const classes = `${matches[i] ? 'grow text-green-500 bg-green-500' : 'wiggle text-red-500 bg-red-500'} bg-opacity-10 rounded`;
+            return <span className={classes}>{char}</span>
           })}
-          <div className="text-grey-800">{" " + text.slice(matches.length)}</div>
+          
         </div>
     </div>
   );
