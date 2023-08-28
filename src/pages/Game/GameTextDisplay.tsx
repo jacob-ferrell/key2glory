1b39f1265fb190c436369c4c2cc4e16d8cfc699c
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-28 17:36:42 -0400

    grouped individual words into their own containers. added bottom border for current key

--- src/pages/Game/GameTextDisplay.tsx
+++ src/pages/Game/GameTextDisplay.tsx
@@ -16,20 +16,42 @@ export default function GameTextDisplay({
 }: GameTextDisplayProps) {
   useEffect(() => {}, [index, matches]);
 
+  function renderText() {
+    let i = 0;
+    const words = text
+      .split(" ")
+      .map((word, i, words) => (i === words.length - 1 ? word : word + " "));
+    return words.map((word) => {
+      return (
+        <div className="flex">
+          {word.split("").map((char) => {
+            char = char == " " ? "\u00A0" : char;
+            const classes = `${
+              i > matches.length - 1
+                ? `text-grey-800 ${i === matches.length ? "border-b-2 border-blue-500" : ""}`
+                : matches[i]
+                ? "grow text-green-500 bg-green-500"
+                : "wiggle text-red-500 bg-red-500"
+            } bg-opacity-10 rounded w-5 flex justify-center items-center text-center`;
+            i++;
+            return <div id={`c-${i}`} className={classes}>{char}</div>;
+          })}
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="flex flex-col justify-center items-center">
-        {isExploading ? <ConfettiExplosion force={0.6} duration={2500} particleCount={80} width={1000} /> : null}
-        <div className="text-2xl flex flex-wrap">
-          {text.split("").map((char, i) => {
-            char = char == " " ? "\u00A0" : char;
-            if (i > matches.length - 1) {
-              return <span className="text-grey-800">{char}</span>
-            }
-            const classes = `${matches[i] ? 'grow text-green-500 bg-green-500' : 'wiggle text-red-500 bg-red-500'} bg-opacity-10 rounded`;
-            return <span className={classes}>{char}</span>
-          })}
-          
-        </div>
+      {isExploading ? (
+        <ConfettiExplosion
+          force={0.6}
+          duration={2500}
+          particleCount={80}
+          width={1000}
+        />
+      ) : null}
+      <div className="text-2xl flex flex-wrap">{renderText()}</div>
     </div>
   );
 }
