94ca376ce603da36e14719c62049a64dbeccfe76
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-29 11:23:05 -0400

    game now uses keypress listeners instead of an input element. fixed stat calculation and added mores stats

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,99 +1,120 @@
-import { ChangeEvent, ClipboardEvent, useEffect, useState, useRef } from "react";
-import GameInput from "./GameInput";
+import {
+  useEffect,
+  useState,
+  useRef,
+} from "react";
 import GameTextDisplay from "./GameTextDisplay";
 import Stopwatch from "./Stopwatch";
 import ClearButton from "./ClearButton";
+import StatsModal from "./StatsModal";
 
 type GameProps = {
   text: string;
 };
-type Stats = { 
-    wpm: number;
-    accuracy: number;
+type Stats = {
+  WPM: number;
+  accuracy: string;
+  time: string;
+  "Keys Missed": number;
 };
 
 export default function Game({ text }: GameProps) {
   const [userInput, setUserInput] = useState("");
-  const [index, setIndex] = useState(0);
   const [matches, setMatches] = useState<boolean[]>([]);
   const [elapsedTime, setElapsedTime] = useState(0.0);
-  const [intervalId, setIntervalId] = useState<number | null>(null);
-  const [stats, setStats] = useState<Stats>({ wpm: 0.0, accuracy: 0 });
+  const [stats, setStats] = useState<Stats>({
+    WPM: 0.0,
+    accuracy: "",
+    time: "",
+    "Keys Missed": 0,
+  });
   const [charactersMissed, setCharactersMissed] = useState<string[]>([]);
   const [isExploading, setIsExploading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  const inputRef = useRef<HTMLInputElement>(null);
   const prevUserInputRef = useRef<string>("");
+  const intervalRef = useRef<number | null>(null);
 
   useEffect(() => {
-    return () => clearInterval(intervalId as unknown as number);
+    return () => clearInterval(intervalRef.current as unknown as number);
   }, []);
 
   useEffect(() => {
-    if (userInput) return;
-    reset();
-  }, [userInput])
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (userInput.length && intervalRef.current === null) {
+      startTimer();
+    }
+    if (!userInput.length && intervalRef.current !== null) {
+      reset();
+    }
+    if (userInput.length === text.length) {
+      return handleCompletion();
+    }
+    if (userInput.length) {
+      handleError();
+    }
+  }, [userInput, intervalRef.current]);
 
   function startTimer() {
-    if (intervalId !== null) return;
-    setIntervalId(
-      setInterval(
-        () => setElapsedTime((prevElapsedTime) => prevElapsedTime + 0.01),
-        10
-      )
+    if (intervalRef.current) return;
+    intervalRef.current = setInterval(
+      () => setElapsedTime((prevElapsedTime) => prevElapsedTime + 0.01),
+      10
     );
   }
 
   function stopTimer() {
-    clearInterval(intervalId as unknown as number);
-    setIntervalId(null);
+    clearInterval(intervalRef.current as unknown as number);
+    intervalRef.current = null;
   }
 
   function resetTimer() {
     setElapsedTime(0.0);
   }
 
-  function handleCompletion(value: string) {
-    if (value !== text) return;
+  function handleCompletion() {
+    if (userInput.length !== text.length) return;
     stopTimer();
-    const newStats = { wpm: getWPM(), accuracy: getAccuracy() };
+    const newStats = {
+      WPM: getWPM(),
+      accuracy: getAccuracy(),
+      time: elapsedTime.toFixed(2) + " seconds",
+      "Keys Missed": charactersMissed.length,
+    };
     setStats(newStats);
     setIsExploading(true);
+    setTimeout(() => {
+      setShowModal(true);
+      reset();
+    }, 1000);
   }
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const { value } = e.target;
-    setIsExploading(false);
-    prevUserInputRef.current = userInput;
-    setUserInput(value);
-    handleError(value);
-    setMatches(getMatchesArray(value));
-    handleCompletion(value);
-    startTimer();
-  }
-
-  function handleError(value: string) {
+  function handleError() {
+    const value = userInput;
+    console.log(value);
     const prev = prevUserInputRef.current;
     const lastCharacterTyped = value[value.length - 1];
     if (isBackSpace(prev, value) || !isError(value)) return;
     console.log("error");
     console.log(lastCharacterTyped);
     setCharactersMissed((prevCharactersMissed) => [
-    ...prevCharactersMissed,
-    text[value.length - 1],
+      ...prevCharactersMissed,
+      text[value.length - 1],
     ]);
-    
   }
 
   function reset() {
     stopTimer();
     resetTimer();
-    setUserInput("");
-    setIndex(0);
-    setElapsedTime(0.0);
     setMatches([]);
     setCharactersMissed([]);
-    inputRef.current?.focus();
+    setUserInput("");
   }
 
   function isBackSpace(prev: string, value: string) {
@@ -104,23 +125,19 @@ export default function Game({ text }: GameProps) {
     return value[value.length - 1] !== text[value.length - 1];
   }
 
-  function handleCopyOrPaste(e: ClipboardEvent<HTMLInputElement>) {
-    e.preventDefault();
-    alert("Copying and pasting is not allowed!");
-  }
-
   function getWPM() {
     let words: number = text.split(" ").length;
     let minutes: number = parseFloat((elapsedTime / 60).toFixed(2));
-    let wpm: number = parseFloat((words / minutes).toFixed(2));
-    console.log(words, minutes, wpm);
-    return wpm;
+    let WPM: number = parseFloat((words / minutes).toFixed(2));
+    console.log(words, minutes, WPM);
+    return WPM;
   }
 
   function getAccuracy() {
-    let accuracy: number = parseFloat(
-      (((text.length - charactersMissed.length) / text.length) * 100).toFixed(2)
-    );
+    let accuracy: string =
+      (((text.length - charactersMissed.length) / text.length) * 100).toFixed(
+        2
+      ) + "%";
     return accuracy;
   }
 
@@ -131,17 +148,30 @@ export default function Game({ text }: GameProps) {
     });
   }
 
+  function handleKeyPress(e: KeyboardEvent) {
+    if (e.key.length > 1 && e.key !== "Backspace") return;
+    e.preventDefault();
+    setUserInput((prevUserInput) => {
+      setIsExploading(false);
+      const newUserInput =
+        e.key.length === 1 ? prevUserInput + e.key : prevUserInput.slice(0, -1);
+      setMatches(getMatchesArray(newUserInput));
+      return newUserInput;
+    });
+  }
+
   return (
-    <div className="flex flex-col items-center gap-12">
-      <Stopwatch elapsedTime={elapsedTime} />
-      <GameTextDisplay text={text} index={index} matches={matches} isExploading={isExploading}/>
-      <GameInput
-        userInput={userInput}
-        handleChange={handleChange}
-        handleCopyOrPaste={handleCopyOrPaste}
-        inputRef={inputRef}
-      />
-      <ClearButton setUserInput={setUserInput} />
-    </div>
+    <>
+      <StatsModal isOpen={showModal} setIsOpen={setShowModal} stats={stats} />
+      <div className="flex flex-col items-center gap-12">
+        <Stopwatch elapsedTime={elapsedTime} />
+        <GameTextDisplay
+          text={text}
+          matches={matches}
+          isExploading={isExploading}
+        />
+        <ClearButton setUserInput={setUserInput} />
+      </div>
+    </>
   );
 }
