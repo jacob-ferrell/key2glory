ce6770a684d71c886be6ddd5321b87d495decd3a
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-09 19:03:57 -0400

    removed unused imports

--- src/pages/Game/GameTextDisplay.tsx
+++ src/pages/Game/GameTextDisplay.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import ConfettiExplosion from "react-confetti-explosion";
 import useTrackLines from "../../hooks/useTrackLines";
 
