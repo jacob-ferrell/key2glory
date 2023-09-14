8798bc352241470f762150b8859e9a4d2d62159f
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-14 10:50:44 -0400

    configured Auth0Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
+        scope: "read:current_user update:current_user_metadata openid profile email"
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
