69e3d10b8640d412521b9af2c2d84f524aa1e4e4
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-30 20:26:44 -0400

    used prettier changed import paths after moving files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,46 @@
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
-import axiosInstance from "../util/axiosInstance.js";
+import axiosInstance from "../api/axiosInstance.js";
 import { AxiosResponse } from "axios";
 
 export default function Navbar() {
-  const { loginWithRedirect, user, isAuthenticated, isLoading, logout, getAccessTokenSilently, getIdTokenClaims } =
-    useAuth0();
+  const {
+    loginWithRedirect,
+    user,
+    isAuthenticated,
+    isLoading,
+    logout,
+    getAccessTokenSilently,
+    getIdTokenClaims,
+  } = useAuth0();
 
-    useEffect(() => {
-      if (!isLoading && !isAuthenticated) return;
-      if (user) {
-        console.log(Object.keys(user));
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) return;
+    if (user) {
+      console.log(Object.keys(user));
+    }
+    getIdTokenClaims().then((res) => console.log(res));
+    getAccessTokenSilently().then((res) => {
+      console.log(res);
+      localStorage.setItem("token", res);
+      interface MyResponseData {
+        // Define the structure of your response data here
+        // For example, if your response data is JSON with a 'message' field:
+        message: string;
       }
-      getIdTokenClaims().then(res => console.log(res)); 
-      getAccessTokenSilently().then(res => {
-        console.log(res);
-        localStorage.setItem("token", res);
-        interface MyResponseData {
-          // Define the structure of your response data here
-          // For example, if your response data is JSON with a 'message' field:
-          message: string;
-        }
-        
-        // ...
-        
-        axiosInstance.get<MyResponseData>("/private/secured").then((res: AxiosResponse<MyResponseData>) => {
+
+      // ...
+
+      axiosInstance
+        .get<MyResponseData>("/private/secured")
+        .then((res: AxiosResponse<MyResponseData>) => {
           // Access the response data
           const responseData: MyResponseData = res.data;
           console.log(responseData);
         });
-      })
-      
-    }, [isAuthenticated])
+    });
+  }, [isAuthenticated]);
   return (
     <div
       className="flex justify-between w-full items-center h-16 text-gray-50 text-lg shadow-sm font-sans font-bold px-6"
@@ -55,7 +63,7 @@ export default function Navbar() {
             onClick={() => logout()}
             className="cursor-pointer rounded transition duration-300 hover:text-purple-600"
           >
-            Log Out {user?.email}
+            Log Out
           </div>
         ) : (
           <div
