f5405556856fd1afca30ba33fbc30dda0c47c38c
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-30 20:37:03 -0400

    updated to get game text through api call using url params

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -3,12 +3,28 @@ import Stopwatch from "./Stopwatch";
 import ClearButton from "./ClearButton";
 import StatsModal from "./StatsModal";
 import useInput from "../../hooks/useInput";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import getTest from "../../api/getTest";
 
-type GameProps = {
+type TypingTest = {
   text: string;
+  id: number;
+  ratings: number;
+  rating: number | null;
+  createdBy: string;
+  currentUserRating: number | null;
+  highScore: null;
 };
 
-export default function Game({ text }: GameProps) {
+export default function Game() {
+  const { testId } = useParams();
+  const [typingTest, setTypingTest] = useState<TypingTest | null>(null);
+
+  useEffect(() => {
+    getTest(testId).then((res) => setTypingTest(res));
+  }, [testId]);
+
   const {
     elapsedTime,
     matches,
@@ -17,15 +33,23 @@ export default function Game({ text }: GameProps) {
     setShowModal,
     stats,
     setUserInput,
-  } = useInput(text);
+  } = useInput(typingTest === null ? null : typingTest.text);
+
 
   return (
     <>
-      <StatsModal isOpen={showModal} setIsOpen={setShowModal} stats={stats} />
+      <StatsModal
+        isOpen={showModal}  
+        setIsOpen={setShowModal}
+        stats={{WPM: 100, accuracy: 98, missedCharacters: [], time: 20, wpmScore: 5, overallScore: 98}}
+        setRating={(rating: number | null) => 
+          setTypingTest(prev => prev ? { ...prev, currentUserRating: rating } : null)
+        }        rating={typingTest === null ? null : typingTest.currentUserRating}
+      />
       <div className="flex flex-col items-center gap-12 h-screen">
         <Stopwatch elapsedTime={elapsedTime} />
         <GameTextDisplay
-          text={text}
+          text={typingTest?.text === null ? "" : typingTest?.text}
           matches={matches}
           isExploading={isExploading}
         />
