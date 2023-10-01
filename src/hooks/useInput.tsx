dbee6b9f909aa51e88c1d648f734624b51373f5b
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-30 22:07:52 -0400

    added sessions

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,18 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import useTimer from "./useTimer";
 import useStats from "./useStats";
+import { useAuth0 } from "@auth0/auth0-react";
+import postScore from "../api/postScore";
+import useSession from "./useSession";
 
-export default function useInput(text: string) {
+
+export default function useInput(text: string | null) {
   const [userInput, setUserInput] = useState("");
   const [matches, setMatches] = useState<boolean[]>([]);
-  const [charactersMissed, setCharactersMissed] = useState<string[]>([]);
+  const [missedCharacters, setMissedCharacters] = useState<string[]>([]);
   const [isExploading, setIsExploading] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   const prevUserInputRef = useRef<string>("");
 
   const timer = useTimer();
-  const stats = useStats({ text, charactersMissed, timer });
+  const stats = useStats({ text, missedCharacters, timer });
+  const { isAuthenticated } = useAuth0();
+  const session = useSession();
+
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
@@ -22,22 +29,36 @@ export default function useInput(text: string) {
   }, []);
 
   useEffect(() => {
-    if (userInput.length && timer.interval === null) { //start timer if user starts typing
+    if (text === null) return;
+    if (userInput.length && timer.interval === null) {
+      //start timer if user starts typing
       timer.start();
+      session.start();
     }
-    if (!userInput.length && timer.interval !== null) { //stop timer if user deletes all input and reset all stats
+    if (!userInput.length && timer.interval !== null) {
+      //stop timer if user deletes all input and reset all stats
       return reset();
     }
-    if (matches[matches.length - 1] === false && isError(userInput) && userInput.length > prevUserInputRef.current.length) { //if user makes 2 mistakes in a row, do not progress but add the missed character
+    if (
+      matches[matches.length - 1] === false &&
+      isError(userInput) &&
+      userInput.length > prevUserInputRef.current.length
+    ) {
+      //if user makes 2 mistakes in a row, do not progress but add the missed character
       addMissedCharacter();
       return setUserInput(prevUserInputRef.current);
     }
-    if (!isBackSpace(prevUserInputRef.current, userInput) && isError(userInput) && userInput !== prevUserInputRef.current) { //add missed character if user makes a mistake
+    if (
+      !isBackSpace(prevUserInputRef.current, userInput) &&
+      isError(userInput) &&
+      userInput !== prevUserInputRef.current
+    ) {
+      //add missed character if user makes a mistake
       addMissedCharacter();
     }
     setMatches(getMatchesArray(userInput));
     if (userInput.length === text.length) {
-      return handleCompletion();
+      handleCompletion();
     }
   }, [userInput, timer.interval]);
 
@@ -46,12 +67,14 @@ export default function useInput(text: string) {
   }
 
   function isError(value: string) {
+    if (text === null) return false;
     return value[value.length - 1] !== text[value.length - 1];
   }
 
   function addMissedCharacter() {
-    setCharactersMissed((prevCharactersMissed) => [
-      ...prevCharactersMissed,
+    if (text === null) return;
+    setMissedCharacters((prev) => [
+      ...prev,
       text[userInput.length - 1],
     ]);
   }
@@ -63,15 +86,30 @@ export default function useInput(text: string) {
     setUserInput((prev) => {
       prevUserInputRef.current = prev;
       const newUserInput =
-      e.key.length === 1 ? prev + e.key : prev.slice(0, -1);
+        e.key.length === 1 ? prev + e.key : prev.slice(0, -1);
       return newUserInput;
     });
   }
 
-  function handleCompletion() {
+  async function handleCompletion() {
     timer.stop();
+    session.stop();
     setIsExploading(true);
-    stats.getStats();
+    // stats.getStats();
+    // const { WPM, accuracy, time, overallScore, wpmScore, missedCharacters } = stats.stats;
+    // if (isAuthenticated && time > 0) {
+    //   await postScore(
+    //     {
+    //       WPM,
+    //       accuracy,
+    //       time,
+    //       overallScore,
+    //       wpmScore,
+    //       missedCharacters
+    //     },
+    //     1
+    //   );
+    // }
     setTimeout(() => {
       setShowModal(true);
       reset();
@@ -81,12 +119,13 @@ export default function useInput(text: string) {
   function reset() {
     timer.reset();
     setMatches([]);
-    setCharactersMissed([]);
+    setMissedCharacters([]);
     setUserInput("");
   }
 
   function getMatchesArray(value: string) {
     //Get an array of booleans where each index represents whether or not the character at that index matches
+    if (text === null) return [];
     return value.split("").map((character, index) => {
       return character === text[index];
     });
