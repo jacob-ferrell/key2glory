706c780a0320f9bbb536320256330c85d407fb22
Jacob Ferrell <michaeljacobferrell@gmail.com>
2024-10-01 19:12:05 -0400

    adds scrict mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,21 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <Auth0Provider
       domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
       authorizationParams={{
         redirect_uri: window.location.origin,
         audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
-        scope: "read:current_user update:current_user_metadata openid profile email"
+        scope:
+          "read:current_user update:current_user_metadata openid profile email",
       }}
     >
       <App />
     </Auth0Provider>
+  </StrictMode>
 );
