bd05b7e2fe17826fd7c70f290798d257481e42f1
Jacob Ferrell <michaeljacobferrell@gmail.com>
2024-10-01 19:29:20 -0400

    creates component containing all filter groups

--- src/pages/BrowseTests/Filters.tsx
+++ src/pages/BrowseTests/Filters.tsx
@@ -1,27 +1,29 @@
 import Filter from "./Filter";
 import filters from "../../common/filters";
-import { useSearchParams } from "react-router-dom";
+import ClearFiltersButton from "./ClearFiltersButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Filters() {
-  const [_, setSearchParams] = useSearchParams();
-  
 
+  const { isAuthenticated } = useAuth0();
+
+  
   return (
-    <div className="w-1/4">
-      <h1>Filters</h1>
-      {filters.map((filter, i) => {
+    <div className="w-fit bg-zinc-800 rounded h-fit px-3 py-2 flex flex-col gap-2">
+      {filters.slice(isAuthenticated ? 0 : 1).map((filter, i) => {
         return (
           <Filter
             key={i}
             name={filter.name}
             options={filter.options}
             queryParam={filter.queryParam}
-            searchParams={_}
-            setSearchParams={setSearchParams}
+            /* searchParams={_}
+            setSearchParams={setSearchParams} */
             allowMultiple={filter.allowMultiple}
           />
         );
       })}
+      <ClearFiltersButton />
     </div>
   );
 }
