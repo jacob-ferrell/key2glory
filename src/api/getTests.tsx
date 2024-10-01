905c5abed3836ad25cf227e9ca42ece29aa28c4b
Jacob Ferrell <michaeljacobferrell@gmail.com>
2024-10-01 19:14:48 -0400

    function for fetching typing tests

--- src/api/getTests.tsx
+++ src/api/getTests.tsx
@@ -2,5 +2,5 @@ import axiosInstance from "./axiosInstance";
 
 export default async function getTests(params: string) {
     const res = await axiosInstance.get("public/typing-test" + params);
-    return res.data.body.content;
+    return res.data.body;
 }
