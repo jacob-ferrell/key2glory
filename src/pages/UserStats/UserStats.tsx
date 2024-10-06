db4bba51dbb643b50367595275377d03f9f6ef55
jacob-ferrell <michaeljacobferrell@gmail.com>
2024-10-06 18:33:53 -0400

    Adds GeneralStats and handles user having no missed characters/stats

--- src/pages/UserStats/UserStats.tsx
+++ src/pages/UserStats/UserStats.tsx
@@ -1,16 +1,34 @@
 import useUserStats from "../../hooks/useUserStats";
 import MissedCharactersTable from "../Game/MissedCharactersTable";
+import GeneralStatsTable from "./GeneralStatsTable";
 
 export default function UserStats() {
-  const { missedCharacters, isEmpty } = useUserStats();
+  const {
+    missedCharacters,
+    hasMissedCharacters,
+    hasGeneralStats,
+  } = useUserStats();
   return (
-    <div>
-      {!isEmpty ? (
-        <MissedCharactersTable
-          missedCharacters={missedCharacters}
-          show={true}
-        />
-      ) : null}
+    <div className="flex justify-center gap-10">
+      <div className="p-3">
+        <h2 className="w-full text-center font-bold">Missed Characters</h2>
+        {!!hasMissedCharacters ? (
+          <MissedCharactersTable
+            missedCharacters={missedCharacters}
+            show={true}
+          />
+        ) : (
+          <div>You have no missed character logs</div>
+        )}
+      </div>
+      <div className="p-3">
+        <h2 className="w-full text-center font-bold">General Stats</h2>
+        {!!hasGeneralStats ? (
+          <GeneralStatsTable />
+        ) : (
+          <div>You have not completed any tests and have no stats</div>
+        )}
+      </div>
     </div>
   );
 }
